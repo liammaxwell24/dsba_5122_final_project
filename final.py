91265a0ca2dd0e5b94106ceaf640321ddbda0f98
liammaxwell24 <128432623+liammaxwell24@users.noreply.github.com>
2023-04-24 12:19:41 -0400

    Update final.py

--- final.py
+++ final.py
@@ -16,20 +16,39 @@ st.markdown(
 "**Statistics for all NBA Players not on rookie contracts for the 2022 NBA Season.**\n"
 )
 
-player = st.multiselect('Select an NBA player', contract_data['name'])
-filtered_contract_data = contract_data[contract_data['name'].isin(player)]
+tab1, tab2 = st.tabs(['Scatter Plot', 'Bar Chart'])
 
 
-st.dataframe(filtered_contract_data)
+with tab1:
+    player = st.multiselect('Select an NBA player', contract_data['name'])
+    filtered_contract_data = contract_data[contract_data['name'].isin(player)]
 
-st.sidebar.header("Pick two player attributes for your scatterplot")
-x_val = st.sidebar.selectbox("Pick your x-axis",filtered_contract_data.select_dtypes(include=np.number).drop(['signed','extended','age at signing/extension','offseason_of_new_contract'], axis=1).columns.tolist())
-y_val = st.sidebar.selectbox("Pick your y-axis",filtered_contract_data.select_dtypes(include=np.number).drop(['signed','extended','age at signing/extension','offseason_of_new_contract'], axis=1).columns.tolist())
 
-scatter = alt.Chart(filtered_contract_data, title=f"{x_val} and {y_val}").mark_point().encode(
-    alt.X(x_val,title=f'{x_val}'),
-    alt.Y(y_val,title=f'{y_val}'),
-    tooltip=['name','new_contract_AAV',x_val, y_val]).configure_mark(
-    opacity=0.5,
-    color='blue')
-st.altair_chart(scatter, theme="streamlit", use_container_width=True)
+    st.dataframe(filtered_contract_data)
+
+    st.sidebar.header("Pick two player attributes for your scatterplot")
+    x_val = st.sidebar.selectbox("Pick your x-axis",filtered_contract_data.select_dtypes(include=np.number).drop(['signed','extended','age at signing/extension','offseason_of_new_contract'], axis=1).columns.tolist())
+    y_val = st.sidebar.selectbox("Pick your y-axis",filtered_contract_data.select_dtypes(include=np.number).drop(['signed','extended','age at signing/extension','offseason_of_new_contract'], axis=1).columns.tolist())
+
+    scatter = alt.Chart(filtered_contract_data, title=f"{x_val} and {y_val}").mark_point().encode(
+        alt.X(x_val,title=f'{x_val}'),
+        alt.Y(y_val,title=f'{y_val}'),
+        tooltip=['name','new_contract_AAV',x_val, y_val]).configure_mark(
+        opacity=0.5,
+        color='blue')
+    st.altair_chart(scatter, theme="streamlit", use_container_width=True)
+    
+with tab2:
+    st.sidebar.header("Pick a player attribute for your bar chart")
+    z_val = st.sidebar.selectbox("Pick your attribute",filtered_contract_data.select_dtypes(include=np.number).drop(['signed','extended','age at signing/extension','offseason_of_new_contract'], axis=1).columns.tolist())
+    count = st.sidebar.slider("Top K Values",min_value=5, max_value=100, value=40, step=10)
+    
+    bar = alt.Chart(contract_data).mark_bar().encode(y = alt.Y('name',title='name', sort = '-x'),
+    x = alt.X(z_val,title=f'{z_val}'), tooltip=['name',z_val,'new_contract_AAV']).transform_window(
+    rank='rank(z_val)',
+    ).transform_filter(
+    (alt.datum.rank < count)
+    )
+    
+
+    st.altair_chart(bar, use_container_width=True) 
